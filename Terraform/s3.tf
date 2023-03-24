e68bc117e799e61dff8a4a3d529a0a9409cd7e58
Ilyas Rufai <rufaiilyas@gmail.com>
2023-03-24 16:41:26 +0100

    update

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -3,13 +3,13 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 locals {
-  files_to_upload = fileset("./images/")
+  files_to_upload = fileset("./images/", "images/*.jpg")
 }
 
 resource "aws_s3_bucket_object" "files" {
-  for_each = { for file in locals.files_to_upload : file => file }
+  for_each = { for file in local.files_to_upload : file => file }
 
-  bucket = aws_s3_bucket.my_bucket.id
+  bucket = aws_s3_bucket.bucket.id
   key    = each.value
   source = "./images/${each.value}"
 }
