3e78610d75fba245a1c7a339a0cc204b90349529
Ilyas Rufai <rufaiilyas@gmail.com>
2023-05-26 08:27:04 +0100

    fix: s3 notification

--- Terraform/sns.tf
+++ Terraform/sns.tf
@@ -1,4 +1,47 @@
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["SNS:Publish"]
+    resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
 resource "aws_sns_topic" "notification" {
+  name   = "s3-event-notification-topic"
+  policy = data.aws_iam_policy_document.policy.json
+}
+
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  topic {
+    topic_arn     = aws_sns_topic.notification.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".jpg"
+  }
+}
+
+resource "aws_sns_topic_subscription" "toemail" {
+  topic_arn = aws_sns_topic.notification.arn
+  protocol  = "email"
+  endpoint  = "rufilboy@gmail.com"
+}
+
+
+
+/* resource "aws_sns_topic" "notification" {
   name = "lambda-gif-bucket-notification"
 }
 
@@ -40,7 +83,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   })
 }
 
-
+ */
 
 
 
